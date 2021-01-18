c7e5e44feb0e1484076e584384a69a29aaca876b
sabrinaspage <sabreyes01@gmail.com>
2021-01-18 13:48:58 -0500

    warmups

---
+++ HackerRank/salesByMatch.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the sockMerchant function below.
+function sockMerchant(n, ar) {
+    var count = 0;
+    var pair = 0;
+    var storeIndex = [];
+
+    for(var i = 0; i < ar.length; i++){
+        if(!storeIndex.includes(i)){
+            for(var j = i + 1; j < ar.length; j++){
+                if(ar[i] == ar[j]){
+                    storeIndex.push(j);
+                    count++;
+                    break;
+                }
+            }
+        }
+    }
+    
+    return count;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const ar = readLine().split(' ').map(arTemp => parseInt(arTemp, 10));
+
+    let result = sockMerchant(n, ar);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
